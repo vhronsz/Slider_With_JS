bc885af920c84d4afd5b60c42a412c7d5d6fa9a5
vhronsz <ryansanjaya290799@gmail.com>
2020-02-18 17:15:43 +0700

    Get component coordinate

--- Js/script.js
+++ Js/script.js
@@ -3,6 +3,11 @@ let pos_y = 0;
 let x = document.getElementById("Container");
 let b = document.getElementById("Content");
 
+window.addEventListener("load",function(e){
+    pos_x = b.offsetLeft
+    pos_y = b.offsetTop
+});
+
 x.addEventListener("mousemove",function(e){
     pos_x += e.movementX;
     pos_y += e.movementY;
