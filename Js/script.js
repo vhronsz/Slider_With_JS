38c6d63d04c54c9b353b3905fc73b7695960ff56
vhronsz <ryansanjaya290799@gmail.com>
2020-02-25 12:21:48 +0700

    added event listener for click

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,6 @@
 let pos_x = 0;
 let pos_y = 0;
+let clicked = false;
 let x = document.getElementById("Container");
 let b = document.getElementById("Content");
 
@@ -9,8 +10,24 @@ window.addEventListener("load",function(e){
 });
 
 x.addEventListener("mousemove",function(e){
-    pos_x += e.movementX;
-    pos_y += e.movementY;
-    $("#Content").css("left",pos_x);
-    $("#Content").css("top",pos_y);
+    if(clicked){
+        pos_x += e.movementX;
+        // pos_y += e.movementY;
+        $("#Content").css("left",pos_x);
+        // $("#Content").css("top",pos_y);
+    }
 });
+
+
+
+x.addEventListener("click",function(){
+    if(clicked == true){
+        clicked = false;  
+    }else{
+        clicked = true;
+    }
+    console.log(clicked);
+});
+
+
+
