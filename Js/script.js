c1358f994f9f818c4737676ed0c8b83c620d8aba
vhronsz <ryansanjaya290799@gmail.com>
2020-02-25 12:50:11 +0700

    rename variable

--- Js/script.js
+++ Js/script.js
@@ -1,32 +1,32 @@
 let pos_x = 0;
 let pos_y = 0;
 let clicked = false;
-let x = document.getElementById("Container");
-let b = document.getElementById("Content");
+let container = document.getElementById("Container");
+let content = document.getElementById("Content");
 
 window.addEventListener("load",function(e){
-    pos_x = b.offsetLeft
-    pos_y = b.offsetTop
+    pos_x = container.offsetLeft
+    pos_y = container.offsetTop 
 });
 
-b.addEventListener("mousemove",function(e){
+container.addEventListener("mousemove",function(e){
     if(clicked){
         pos_x += e.movementX;
-        // pos_y += e.movementY;
+        pos_y += e.movementY;
         $("#Content").css("left",pos_x);
-        // $("#Content").css("top",pos_y);
+        $("#Content").css("top",pos_y);
     }
 });
 
-x.addEventListener("mouseleave",function(e){
+container.addEventListener("mouseleave",function(e){
    clicked = false;
 });
 
-b.addEventListener("mousedown",function(){
+container.addEventListener("mousedown",function(){
     clicked = true;
 });
 
-b.addEventListener("mouseup",function(){
+container.addEventListener("mouseup",function(){
     clicked = false;
 });
 
